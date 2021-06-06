cf55af5ca1effa941b99419d9c86bfd898a9b089
Gem Xli <x.li.gem@gmail.com>
2021-06-06 21:49:19 +0800

    fix: render svg

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,60 @@
   <div class="canvas">
     <canvas ref="cc" width="600" height="400"></canvas>
   </div>
-  <br />
+  <div class="list">
+    <img src="images/style-1.svg" @click="changeName('style-1')" />
+    <img src="images/style-2.svg" @click="changeName('style-2')" />
+  </div>
   <label class="pl-5">
     <span>width: </span>
     <input type="number" v-model="data.width" />
   </label>
-  <label class="pl-5">
-    <span>height: </span>
-    <input type="number" v-model="data.height" />
-  </label>
 
-  <button @click="generateImg" class="pl-5">generate</button>
   <button @click="download" class="pl-5">download</button>
+  <div hidden ref="hiddenDiv"></div>
 </template>
 
 <script lang="ts" setup>
-import { Canvg } from 'canvg'
 import { reactive, ref } from '@vue/reactivity'
-import { onMounted } from '@vue/runtime-core'
+import { onMounted, watch } from '@vue/runtime-core'
 import { saveAs } from 'file-saver'
 
 const data = reactive({
-  width: 350,
-  height: 350,
-  src: ''
+  width: 470,
+  name: 'style-1'
 })
 
+const hiddenDiv = ref<HTMLDivElement | null>(null)
 const cc = ref<HTMLCanvasElement | null>(null)
 
 onMounted(async () => {
   const url = new URL(location.href)
   const size = url.searchParams.get('size')
+  const name = url.searchParams.get('name')
+
+  if (name) {
+    data.name = name
+  }
 
   if (size) {
-    const [w, h] = size.split('x')
-
-    data.width = parseInt(w) || data.width
-    data.height = parseInt(h) || data.width || data.height
+    data.width = parseInt(size) || data.width
   }
 
   await generateImg()
 
-  if (url.searchParams.get('d')) {
+  if (url.searchParams.get('download')) {
     download()
   }
 })
+
+watch(
+  () => [data.width, data.name],
+  () => generateImg()
+)
+
+function changeName(n: string) {
+  data.name = n
+}
 
 async function generateImg() {
   const canvas = cc.value
@@ -54,16 +63,34 @@ async function generateImg() {
     return
   }
 
-  const n = new URL(location.href).searchParams.get('n') || 'style-1'
-  const svgFile = `./images/${n}.svg`
+  const svgFile = `./images/${data.name}.svg`
 
-  const ctx = canvas.getContext('2d')!
-  const v = await Canvg.from(ctx, svgFile)
+  const img = new Image()
 
-  v.resize(data.width, data.height, 'xMidYMid meet')
+  return new Promise<void>(async (resolve, reject) => {
+    const res = await fetch(svgFile)
+    const svg = await res.text()
+    hiddenDiv.value!.innerHTML = svg
+    const svgEl = hiddenDiv.value!.querySelector('svg')
+    svgEl?.setAttribute('width', data.width.toString())
+    svgEl?.setAttribute('height', data.width.toString())
 
-  // Render only first frame, ignoring animations and mouse.
-  await v.render()
+    const svgData = new XMLSerializer().serializeToString(svgEl!)
+    const blob = new Blob([svgData], { type: 'image/svg+xml' })
+
+    img.onload = () => {
+      canvas.width = data.width
+      canvas.height = data.width
+      canvas.getContext('2d')!.drawImage(img, 0, 0)
+      resolve()
+    }
+
+    img.onerror = (e) => {
+      reject(new Error('Render failed!: ' + e))
+    }
+
+    img.src = window.URL.createObjectURL(blob)
+  })
 }
 
 function download() {
@@ -84,12 +111,25 @@ function download() {
 }
 
 .canvas {
-  height: 600px;
+  height: 500px;
   overflow: auto;
+  border-bottom: 1px solid gray;
 }
 
 canvas {
   border: 1px solid gray;
+}
+
+.list {
+  height: 100px;
+  margin: 10px 0;
+}
+
+.list img {
+  width: 100px;
+  height: 100px;
+  margin-right: 10px;
+  cursor: pointer;
 }
 
 .pl-5 {
